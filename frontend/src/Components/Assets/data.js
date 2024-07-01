15638389508e892fb023e3c3a21661ef1102f688
SHASWAT2556 <tshaswat32@gmail.com>
2024-07-02 00:06:52 +0530

    Update data.js

--- frontend/src/Components/Assets/data.js
+++ frontend/src/Components/Assets/data.js
@@ -4,27 +4,27 @@ import p3_img from './product_3.png'
 import p4_img from './product_4.png'
 
 let data_product = [
-  {
-    id:1,
-    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+  
+  {id:1,
+    name:"Brown Fur Coat and black and White Floral Skirt",
     image:p1_img,
     new_price:50.00,
     old_price:80.50,
   },
   {id:2,
-    name:"Pink Bodyfit Blouse",
+    name:"Baby Pink Solid Short Sleeves Regular Top",
     image:p2_img,
     new_price:85.00,
     old_price:120.50,
   },
   {id:3,
-    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name:"Brown Crop Top",
     image:p3_img,
     new_price:60.00,
     old_price:100.50,
   },
   {id:4,
-    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name:"Fancy Top",
     image:p4_img,
     new_price:100.00,
     old_price:150.00,
@@ -32,4 +32,4 @@ let data_product = [
   
 ];
 
-export default data_product;
+export default data_product;
